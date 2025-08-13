bd948a06ae153837e7a74c173a33c28d1939d523
Hans Queja <hansqueja8@gmail.com>
2025-08-13 17:02:33 +0800

    Add nextjs setup files

---
+++ frontend-next/components/Map.js
@@ -0,0 +1,27 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+ 
+// fix default icon issue in leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.3/images/marker-shadow.png'
+});
+ 
+export default function Map({ positions }) {
+  const center = [14.5995, 120.9842]; // Manila default
+  return (
+    <MapContainer center={center} zoom={12} style={{ height: "80vh", width: "100%" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {positions.map((p, i) => (
+        <Marker key={i} position={[p.latitude, p.longitude]}>
+          <Popup>
+            Device {p.deviceId}<br />Speed: {p.speed}
+          </Popup>
+        </Marker>
+  	))}
+	</MapContainer>
+  );
+}
