4661f109ec2b9df1f48552d4bd083c1496029b67
Hans Queja <hansqueja8@gmail.com>
2025-08-13 16:15:27 +0800

    Add routes for traccar controller

--- backend-laravel/routes/api.php
+++ backend-laravel/routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\TraccarController;
+
+Route::get('/traccar/devices', [TraccarController::class, 'devices']);
+Route::get('/traccar/positions', [TraccarController::class, 'positions']);
